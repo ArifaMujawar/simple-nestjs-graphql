a37eae2d264a820bad78d621f10e65d58ddb32db
Arifa <arifabegam.mujawar@gmail.com>
2020-10-31 20:06:37 +0200

    Add a simple graphql Query

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,18 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { GraphQLModule } from '@nestjs/graphql';
+import { join } from 'path';
+import { AuthorsModule } from './authors/authors.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    GraphQLModule.forRoot({
+      typePaths: ['./**/*.graphql'],
+      definitions: {
+        path: join(process.cwd(), 'src/graphql.ts'),
+        outputAs: 'class',
+      },
+    }),
+    AuthorsModule,
+  ],
 })
 export class AppModule {}
---
+++ src/authors/authors.service.ts
@@ -0,0 +1,43 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class AuthorsService {
+  authorsDatabase: {
+    id: number;
+    firstName: string;
+    lastName: string;
+    phoneNumber: string;
+    address: string;
+  }[];
+
+  constructor() {
+    this.authorsDatabase = [
+      {
+        id: 10,
+        firstName: 'John',
+        lastName: 'Doe',
+        phoneNumber: '+358 1231',
+        address: 'Kallio, Espoo',
+      },
+      {
+        id: 11,
+        firstName: 'Saphire',
+        lastName: 'Italiano',
+        phoneNumber: '+358 23121',
+        address: 'Leppavaara, Espoo',
+      },
+      {
+        id: 12,
+        firstName: 'Maria',
+        lastName: 'Vachini',
+        phoneNumber: '+12 23451',
+        address: 'Adelaide, Australia',
+      },
+    ];
+  }
+
+  findOneById(id: number) {
+    const result = this.authorsDatabase.filter(author => author.id === id);
+    return result[0];
+  }
+}
